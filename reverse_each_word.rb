7ecd3ce277323390c1862f47dc084a099e6d81b5
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-14 20:50:32 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def reverse_each_word(string)
+   new_array =[]
+  array = string.split("")
+  binding.pry
+array.each do |words|
+   new_array << words.reverse
+  end
+  return new_array.join("")
+end
+
+  
+  
